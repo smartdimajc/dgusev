2285a5f89e6c79fd735ffd558ef7204f3fc77e14
Dmitry Gusev <smartdima@rambler.ru>
2018-03-03 15:29:51 +0300

    1.2. Подключить сборщик Maven[#4086]

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+/** главный класс калькулятора
+
+*/
+public class Calculate {
+
+    public static void main(String[] arg) {
+    System.out.println("Calculate");
+    int first =Integer.valueOf(arg[0]);
+    int second=Integer.valueOf(arg[1]);
+    int summ=first-second;
+    System.out.println("Sum: "+summ);    
+
+
+    }
+    }
