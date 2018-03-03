1aaa1a525073b82b6d92fb64c83d2b938a53e8c7
Dmitry Gusev <smartdima@rambler.ru>
2018-03-03 13:37:39 +0300

    1.1. Создание Calculate

---
+++ src/ru/job4j/Calculate.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Calculate {
+
+    public static void main(String[] arg) {
+    System.out.println("Calculate");
+    int first =Integer.valueOf(arg[0]);
+    int second=Integer.valueOf(arg[1]);
+    int summ=first-second;
+    System.out.println("Sum: "+summ);    
+
+
+    }
+    }
