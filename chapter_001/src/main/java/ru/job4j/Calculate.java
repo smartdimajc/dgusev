a723f825efbc76934f005b5487e8ef4bd94f7dcc
Dmitry Gusev <smartdima@rambler.ru>
2018-03-17 16:44:42 +0300

    1.1. Создание Hello World[#141]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,31 +1,21 @@
 package ru.job4j;
 /** главный класс калькулятора
  *
- *Calculate.
+ *Calculate. Решение задачи в части 001 урок 1.
  *
  *@author Gusev Dmitry (smartdima@rambler.ru)
+ *@since 05.11.2018
  */
 public class Calculate {
+
     /**
 	* Main
-	* @param args - argumens of command line
+	* @param args - arguments of command line
 	*/
-
+ 
     public static void main(String[] arg) {
-    System.out.println("Calculate");
-	/**
-	 * first  - 1 input value
-	 * second - 2 input value
-	 * summ   - result value
-	 */
-    private int first =Integer.valueOf(arg[0]);
-    private int second=Integer.valueOf(arg[1]);
-    private int summ=first-second;
-	/**
-	*@return Return sum of tow arguments.
-	*/
-    System.out.println("Sum: "+summ);    
-
-
+    System.out.println("Hello world");
+	
+	
     }
     }
